13099c0d109eb8c57a1c141f21ebdcbb93cbb023
Nathan Tefft <>
2019-05-14 16:53:32 -0400

    Update lpdtutil.py

--- lpdtutil.py
+++ lpdtutil.py
@@ -36,20 +36,25 @@ def veh_dr_drinking_status(df_vehicle, df_driver, drinking_definition = 'mi', ba
         df_driver_drink_status = df_driver_drink_status.rename(columns={'mibac1':'drink_status1','mibac2':'drink_status2','mibac3':'drink_status3','mibac4':'drink_status4','mibac5':'drink_status5','mibac6':'drink_status6','mibac7':'drink_status7','mibac8':'drink_status8','mibac9':'drink_status9','mibac10':'drink_status10',}) # rename columns 
     elif drinking_definition == 'police_report_only':
         df_driver_drink_status = df_veh_driver['drinking']
+        df_driver_drink_status = df_driver_drink_status.rename('drink_status')
     elif drinking_definition == 'any_evidence':
         df_driver_drink_status = df_veh_driver['dr_drink']
+        df_driver_drink_status = df_driver_drink_status.rename('drink_status')
     elif drinking_definition == 'police_report_primary':
         df_driver_drink_status = df_veh_driver['drinking']
         df_driver_drink_status.loc[(df_veh_driver['alcohol_test_result']==0) & (df_veh_driver['drinking'].isin([8,9]) | df_veh_driver['drinking'].isna())] = 0
         df_driver_drink_status.loc[(df_veh_driver['alcohol_test_result']>0) & (df_veh_driver['drinking'].isin([8,9]) | df_veh_driver['drinking'].isna())] = 1         
+        df_driver_drink_status = df_driver_drink_status.rename('drink_status')
     elif drinking_definition == 'bac_test_primary':
         df_driver_drink_status = df_veh_driver['drinking']
         df_driver_drink_status.loc[df_veh_driver['alcohol_test_result']==0] = 0
         df_driver_drink_status.loc[df_veh_driver['alcohol_test_result']>bac_threshold_scaled] = 1
+        df_driver_drink_status = df_driver_drink_status.rename('drink_status')
     elif drinking_definition == 'impaired_vs_sober':
         df_driver_drink_status = pandas.Series(index=df_veh_driver.index)
         df_driver_drink_status.loc[(df_veh_driver['alcohol_test_result']==0) | (df_veh_driver['dr_drink']==0)] = 0
         df_driver_drink_status.loc[(df_veh_driver['alcohol_test_result']>=bac_threshold_scaled) & (df_veh_driver['dr_drink']!=0)] = 1
+        df_driver_drink_status = df_driver_drink_status.rename('drink_status')
     
     return df_driver_drink_status
 
@@ -88,9 +93,80 @@ def state_year_prop_miss(miss_any):
     df_acc_miss_any = df_accident.merge(miss_any,how='inner',on=['year','st_case'])
     return df_acc_miss_any[['state','miss_any']].groupby(['year','state']).mean()
     
-    
-# code for testing
-df_acc_miss_flag = accident_missing_data(df_accident,df_vehicle,get_driver(df_person),drinking_definition='any_evidence')
-df_acc_miss_flag['miss_any'].value_counts()
+#    
+## code for testing
+#df_acc_miss_flag = accident_missing_data(df_accident,df_vehicle,get_driver(df_person),drinking_definition='any_evidence')
+#df_acc_miss_flag['miss_any'].value_counts()
+#
+#test = state_year_prop_miss(df_acc_miss_flag['miss_any'])
 
-test = state_year_prop_miss(df_acc_miss_flag['miss_any'])
+
+first_year = 2016
+last_year = 2017
+earliest_hour=20
+latest_hour=4
+drinking_definition = 'any_evidence'
+state_year_prop_threshold = 0.13
+equal_mixing=['year','state','weekend','hour']
+def get_lpdt_estimation_sample(df_accident, df_vehicle, df_person, first_year=2017, last_year=2017, earliest_hour=20, 
+                               latest_hour=4, equal_mixing=['year','state','weekend','hour'], drinking_definition='mi', 
+                               bac_threshold = 0.08, state_year_prop_threshold = 0.13):
+    
+    df_accident_est = df_accident.loc[range(first_year,last_year+1)] # restrict sample to selected years
+    print('Count of accidents after year sample restriction: ')
+    print(len(df_accident_est.index))
+    if earliest_hour > latest_hour: # wrap selected hours across midnight, and keep that sample
+        df_accident_est = df_accident_est.loc[(df_accident_est['hour']>=earliest_hour) | (df_accident_est['hour']<=latest_hour)]
+    else: # get simple range of hours, and keep that sample
+        df_accident_est = df_accident_est.loc[(df_accident_est['hour']>=earliest_hour) & (df_accident_est['hour']<=latest_hour)]
+    print('Count of accidents after hour sample restriction: ')
+    print(len(df_accident_est.index))
+    
+    # keep only accidents with 1 or 2 involved vehicles
+    acc_veh_count = df_vehicle[df_vehicle.index.droplevel('veh_no').isin(df_accident_est.index)].groupby(['year','st_case']).size() # series that counts vehicles in each accident
+    acc_veh_count = acc_veh_count.rename('acc_veh_count')
+    print('Count of accidents by vehicles per accident:')
+    print(acc_veh_count.value_counts())
+    df_accident_est = df_accident_est.merge(acc_veh_count.loc[acc_veh_count<=2],how='inner',on=['year','st_case'])
+    print('Count of accidents after vehicle count sample restriction: ')
+    print(len(df_accident_est.index))
+    
+    # get dataframe of booleans indicating whether each variable has missing data (or all of them)
+    df_acc_miss_flag = accident_missing_data(df_accident_est,
+                                             df_vehicle[df_vehicle.index.droplevel('veh_no').isin(df_accident_est.index)],
+                                             get_driver(df_person[df_person.index.droplevel(['veh_no','per_no']).isin(df_accident_est.index)]),
+                                             drinking_definition)
+    df_acc_miss_flag['miss_any'].value_counts()
+    df_st_yr_prop_miss = state_year_prop_miss(df_acc_miss_flag['miss_any'])
+    
+    # only keep accidents in state-years that have a proportion of missing data that is above the given threshold
+    df_accident_est = df_accident_est.reset_index().set_index(['year','state']) # reset index in order to select by state and year
+    df_accident_est = df_accident_est[df_accident_est.index.isin(df_st_yr_prop_miss.loc[df_st_yr_prop_miss['miss_any']<state_year_prop_threshold].index)]
+    df_accident_est = df_accident_est.reset_index().set_index(['year','st_case'])
+    print('Count of accidents after state-year missing proportion sample restriction: ')
+    print(len(df_accident_est.index))
+    
+    # only keep accidents that don't have missing data
+    df_accident_est = df_accident_est[df_accident_est.index.isin(df_acc_miss_flag.loc[df_acc_miss_flag['miss_any']==False].index)]
+    print('Count of accidents after state-year missing data sample restriction: ')
+    print(len(df_accident_est.index))
+    
+    # generate weekend variable
+    df_accident_est['weekend'] = ((df_accident_est['day_week'] == 6) & (df_accident_est['hour'] >= 20)) | (df_accident_est['day_week'] == 7) | ((df_accident_est['day_week'] == 1) & (df_accident_est['hour'] <= 4))
+    print('Count of weekdays vs weekend days: ')
+    print(df_accident_est['weekend'].value_counts())
+    
+    # get dataframe of drinking status, then group by accident to sum drunk counts
+    df_acc_drink_count = veh_dr_drinking_status(df_vehicle[df_vehicle.index.droplevel('veh_no').isin(df_accident_est.index)], 
+                                             get_driver(df_person[df_person.index.droplevel(['veh_no','per_no']).isin(df_accident_est.index)]), 
+                                             drinking_definition).groupby(['year','st_case']).sum()
+    
+    # merge in drinking status and collapse accidents by number of drinkers and number of vehicles per accident
+    df_accident_est = df_accident_est.merge(df_acc_drink_count,how='left',on=['year','st_case'])
+    df_accident_est = df_accident_est.reset_index().set_index(equal_mixing)[['acc_veh_count','drink_status']] # reset index to equal mixing variables and keep vehicle count and drunk driving variables
+    
+    # create interactions of categories of acc_veh_count and drink_status
+    for col in ['acc_veh_count', 'drink_status']:
+        df_accident_est[col] = df_accident_est[col].astype('category')
+    
+    test = pandas.get_dummies(df_accident_est)
