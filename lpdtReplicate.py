fde4c834c1df2ce2c2a5fd194ccb50f71629b821
Nathan Tefft <>
2019-10-27 16:51:06 -0600

    Finishing summary statistics

--- lpdtReplicate.py
+++ lpdtReplicate.py
@@ -28,24 +28,24 @@ df_person.set_index(['year','st_case','veh_no','per_no'],inplace=True) # set the
 bsreps = 3
 sy_p_t = 0.13 # the value that best approximates L&P's results
 
-# EXAMPLE REGULAR ESTIMATION
-analytic_sample = lpdtUtil.get_analytic_sample(df_accident,df_vehicle,df_person,1983,1993,20,4,'any_evidence',
-                    bac_threshold=0,state_year_prop_threshold=sy_p_t,mirep=False,summarize_sample=True)
-mod_res = lpdtFit.fit_model(analytic_sample,df_vehicle,df_person,['year','state','weekend','hour'],bsreps)
-print(mod_res.final_params)
-
-# EXAMPLE MULTIPLE IMPUTATION ESTIMATION
-mod_results = lpdtFit.fit_model_mi(df_accident,df_vehicle,df_person,1983,1993,20,4,['year','state','weekend','hour'],'any_evidence',
-                    bac_threshold=0,state_year_prop_threshold=sy_p_t,bsreps=bsreps,mireps=10)
-print(mod_results.mi_params)
-print(mod_results.mi_llf)
-print(mod_results.mi_df_resid)
+## EXAMPLE REGULAR ESTIMATION
+#analytic_sample = lpdtUtil.get_analytic_sample(df_accident,df_vehicle,df_person,1983,1993,20,4,'any_evidence',
+#                    bac_threshold=0,state_year_prop_threshold=sy_p_t,mirep=False,summarize_sample=True)
+#mod_res = lpdtFit.fit_model(analytic_sample,df_vehicle,df_person,['year','state','weekend','hour'],bsreps)
+#print(mod_res.final_params)
+#
+## EXAMPLE MULTIPLE IMPUTATION ESTIMATION
+#mod_results = lpdtFit.fit_model_mi(df_accident,df_vehicle,df_person,1983,1993,20,4,['year','state','weekend','hour'],'any_evidence',
+#                    bac_threshold=0,state_year_prop_threshold=sy_p_t,bsreps=bsreps,mireps=10)
+#print(mod_results.mi_params)
+#print(mod_results.mi_llf)
+#print(mod_results.mi_df_resid)
 
 # Data for Table 1: Outline of LP Replication Exercise  (can ignore the estimation section, since Table 1 reports summary statistics)
 analytic_sample = lpdtUtil.get_analytic_sample(df_accident,df_vehicle,df_person,1983,1993,20,4,'police_report_only',
                     bac_threshold=0,state_year_prop_threshold=sy_p_t,mirep=False,summarize_sample=True)
 
-# Data for bottom of Table 1: Outline of LP Replication Exercise  (definition has changed to definition 5, which runs the supplemental analysis, so need only look at the section "FOR BOTTOM HALF OF TABLE 1" )
+# Data for item 9 of Table 1: Outline of LP Replication Exercise  (definition has changed to definition 5, which runs the supplemental analysis, so need only look at the section "FOR BOTTOM HALF OF TABLE 1" )
 analytic_sample = lpdtUtil.get_analytic_sample(df_accident,df_vehicle,df_person,1983,1993,20,4,'impaired_vs_sober',
                     bac_threshold=0,state_year_prop_threshold=sy_p_t,mirep=False,summarize_sample=True)
 
@@ -53,12 +53,26 @@ analytic_sample = lpdtUtil.get_analytic_sample(df_accident,df_vehicle,df_person,
 analytic_sample = lpdtUtil.get_analytic_sample(df_accident,df_vehicle,df_person,1983,1993,20,4,'any_evidence',
                     bac_threshold=0,state_year_prop_threshold=sy_p_t,mirep=False,summarize_sample=True)
 
-# Data for Table 4 (top portion)
+# Data for Table 4 (top portion, definitions 1 through 4)
+# Definition 1
+analytic_sample = lpdtUtil.get_analytic_sample(df_accident,df_vehicle,df_person,1983,1993,20,4,'police_report_only',
+                    bac_threshold=0,state_year_prop_threshold=1,mirep=False,summarize_sample=True)
+# Definition 2
 analytic_sample = lpdtUtil.get_analytic_sample(df_accident,df_vehicle,df_person,1983,1993,20,4,'any_evidence',
-                    bac_threshold=0,state_year_prop_threshold=0.2,mirep=False,summarize_sample=True)
+                    bac_threshold=0,state_year_prop_threshold=1,mirep=False,summarize_sample=True)
+# Definition 3
+analytic_sample = lpdtUtil.get_analytic_sample(df_accident,df_vehicle,df_person,1983,1993,20,4,'police_report_primary',
+                    bac_threshold=0,state_year_prop_threshold=1,mirep=False,summarize_sample=True)
+# Definition 4
+analytic_sample = lpdtUtil.get_analytic_sample(df_accident,df_vehicle,df_person,1983,1993,20,4,'bac_test_primary',
+                    bac_threshold=0,state_year_prop_threshold=1,mirep=False,summarize_sample=True)
+
+# Data for Table 4 (last column, optimized to match L&P for missing state-years)
+analytic_sample = lpdtUtil.get_analytic_sample(df_accident,df_vehicle,df_person,1983,1993,20,4,'any_evidence',
+                    bac_threshold=0,state_year_prop_threshold=0.13,mirep=False,summarize_sample=True)
 
 # Data for Table 4 (bottom portion)
 analytic_sample = lpdtUtil.get_analytic_sample(df_accident,df_vehicle,df_person,1983,1993,20,4,'impaired_vs_sober',
-                    bac_threshold=0,state_year_prop_threshold=0.2,mirep=False,summarize_sample=True)
+                    bac_threshold=0.10,state_year_prop_threshold=1,mirep=False,summarize_sample=True)
 analytic_sample = lpdtUtil.get_analytic_sample(df_accident,df_vehicle,df_person,1983,1993,20,4,'impaired_vs_sober',
-                    bac_threshold=0,state_year_prop_threshold=0.13,mirep=False,summarize_sample=True)
+                    bac_threshold=0.10,state_year_prop_threshold=0.13,mirep=False,summarize_sample=True)
