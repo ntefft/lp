5a52a6da46dc9e4d2c649c46c5bbe6f9fc3b6428
Nathan Tefft <>
2019-11-05 20:25:47 -0700

    final testing of file reorganization

--- replication/replicate.py
+++ replication/replicate.py
@@ -24,15 +24,15 @@ df_person = pandas.read_csv('replication\\data\\df_person.csv')
 df_person.set_index(['year','st_case','veh_no','per_no'],inplace=True) # set the index
 
 # set estimation parameters
-bsreps = 2 # bootstrap replicates
-#bsreps = 100 # bootstrap replicates
-mireps = 2 # multiple imputation replicates to use (FARS includes a total of 10)
-#mireps = 10 # multiple imputation replicates to use (FARS includes a total of 10)
+#bsreps = 1 # bootstrap replicates for testing
+bsreps = 100 # bootstrap replicates
+#mireps = 2 # multiple imputation replicates to use, for testing
+mireps = 10 # multiple imputation replicates to use (FARS includes a total of 10)
 sy_p_t = 0.13 # state-year proportion missing threshold that best approximates L&P's results
 # drinking definitions 1 through 4
 drink_defs = ['police_report_only','any_evidence','police_report_primary','bac_test_primary']
 #results_folder = 'replication\\results'
-results_folder = 'replication\\temp'
+results_folder = 'replication\\temp' # for testing
 if not os.path.exists(results_folder):
         os.makedirs(results_folder) # generate results directory, if it doesn't exist
 
@@ -104,20 +104,21 @@ res_fmt_df.T.to_excel(results_folder + '\\table5_panel2.xlsx') # Note: should fo
 equal_mixings = [['all'],['hour'],['year','hour'],['year','weekend','hour'],['year','state','hour'],['year','state','weekend','hour']]
 for drink_def in drink_defs: 
     res_fmt = list() # list of results, formatted
+    analytic_sample = util.get_analytic_sample(df_accident,df_vehicle,df_person,1983,1993,20,4,drink_def,
+                    bac_threshold=0,state_year_prop_threshold=sy_p_t,mireps=False,summarize_sample=False)
     for eq_mix in equal_mixings: 
         print("Estimating model for drinking definition: " + drink_def) 
-        analytic_sample = lpUtil.get_analytic_sample(df_accident,df_vehicle,df_person,1983,1993,20,4,drink_def,
-                            bac_threshold=0,state_year_prop_threshold=sy_p_t,mireps=False,summarize_sample=False)
-        mod_res = lpFit.fit_model(analytic_sample,df_vehicle,df_person,eq_mix,bsreps)
+        mod_res = estimate.fit_model(estimate.get_estimation_sample(analytic_sample,eq_mix,2),bsreps)
         res_fmt.append([eq_mix,round(mod_res.final_params[0][0],2),'('+str(round(mod_res.final_params[0][1],2))+')',
                      round(mod_res.final_params[1][0],2),'('+str(round(mod_res.final_params[1][1],2))+')',(mod_res.df_resid+2)])
     res_fmt_df = pandas.DataFrame(res_fmt,columns=['eq_mix','theta','theta_se','lambda','lambda_se','total_dof'])
     res_fmt_df.T.to_excel(results_folder + '\\tableA1_panel_' + drink_def + '.xlsx') # Note: should format as text after opening Excel file    
 res_fmt = list() # list of results, formatted
+analytic_sample = util.get_analytic_sample(df_accident,df_vehicle,df_person,1983,1993,20,4,'bac_test_primary',
+                        bac_threshold=0,state_year_prop_threshold=sy_p_t,mireps=mireps,summarize_sample=False)
 for eq_mix in equal_mixings: 
-    print("Estimating multiple imputation model:") 
-    mod_res = lpFit.fit_model_mi(df_accident,df_vehicle,df_person,1983,1993,20,4,eq_mix,'bac_test_primary',
-                        bac_threshold=0,state_year_prop_threshold=sy_p_t,bsreps=bsreps,mireps=mireps)
+    print("Estimating multiple imputation model:")     
+    mod_res = estimate.fit_model_mi(analytic_sample,eq_mix,2,bsreps,mireps)
     res_fmt.append([eq_mix,round(mod_res.mi_params[0][0],2),'('+str(round(mod_res.mi_params[0][1],2))+')',
                      round(mod_res.mi_params[1][0],2),'('+str(round(mod_res.mi_params[1][1],2))+')',(mod_res.mi_df_resid+2)])
 res_fmt_df = pandas.DataFrame(res_fmt,columns=['eq_mix','theta','theta_se','lambda','lambda_se','total_dof'])
@@ -128,9 +129,9 @@ for drink_def in drink_defs:
     res_fmt = list() # list of results, formatted
     for yr in range(1983,1994): 
         print("Estimating model for drinking definition " + drink_def + " in year " + str(yr)) 
-        analytic_sample = lpUtil.get_analytic_sample(df_accident,df_vehicle,df_person,yr,yr,20,4,drink_def,
-                            bac_threshold=0,state_year_prop_threshold=sy_p_t,mireps=False,summarize_sample=False)
-        mod_res = lpFit.fit_model(analytic_sample,df_vehicle,df_person,['year','state','weekend','hour'],bsreps)
+        analytic_sample = util.get_analytic_sample(df_accident,df_vehicle,df_person,yr,yr,20,4,drink_def,
+                    bac_threshold=0,state_year_prop_threshold=sy_p_t,mireps=False,summarize_sample=False)
+        mod_res = estimate.fit_model(estimate.get_estimation_sample(analytic_sample,['year','state','weekend','hour'],2),bsreps)
         res_fmt.append([yr,round(mod_res.final_params[0][0],2),'('+str(round(mod_res.final_params[0][1],2))+')',
                      round(mod_res.final_params[1][0],2),'('+str(round(mod_res.final_params[1][1],2))+')',(mod_res.df_resid+2)])
     res_fmt_df = pandas.DataFrame(res_fmt,columns=['year','theta','theta_se','lambda','lambda_se','total_dof'])
@@ -145,9 +146,9 @@ for drink_def in drink_defs:
         else:
             earliest_hour = earliest_hour_raw
         print("Estimating model for drinking definition " + drink_def + " in hour " + str(earliest_hour)) 
-        analytic_sample = lpUtil.get_analytic_sample(df_accident,df_vehicle,df_person,1983,1993,earliest_hour,earliest_hour,drink_def,
-                            bac_threshold=0,state_year_prop_threshold=sy_p_t,mireps=False,summarize_sample=False)
-        mod_res = lpFit.fit_model(analytic_sample,df_vehicle,df_person,['year','state','weekend','hour'],bsreps)
+        analytic_sample = util.get_analytic_sample(df_accident,df_vehicle,df_person,1983,1993,earliest_hour,earliest_hour,drink_def,
+                    bac_threshold=0,state_year_prop_threshold=sy_p_t,mireps=False,summarize_sample=False)
+        mod_res = estimate.fit_model(estimate.get_estimation_sample(analytic_sample,['year','state','weekend','hour'],2),bsreps)
         res_fmt.append([earliest_hour,round(mod_res.final_params[0][0],2),'('+str(round(mod_res.final_params[0][1],2))+')',
                      round(mod_res.final_params[1][0],2),'('+str(round(mod_res.final_params[1][1],2))+')',(mod_res.df_resid+2)])
     res_fmt_df = pandas.DataFrame(res_fmt,columns=['hour','theta','theta_se','lambda','lambda_se','total_dof'])
@@ -158,9 +159,9 @@ drink_def = 'police_report_primary'
 res_fmt = list() # list of results, formatted
 for yr in range(1983,1994): 
     print("Estimating model for drinking definition " + drink_def + " in year " + str(yr)) 
-    analytic_sample = lpUtil.get_analytic_sample(df_accident,df_vehicle,df_person,yr,yr,20,4,drink_def,
-                        bac_threshold=0,state_year_prop_threshold=sy_p_t,mireps=False,summarize_sample=False)
-    mod_res = lpFit.fit_model(analytic_sample,df_vehicle,df_person,['year','state','weekend','hour'],bsreps)
+    analytic_sample = util.get_analytic_sample(df_accident,df_vehicle,df_person,yr,yr,20,4,drink_def,
+                    bac_threshold=0,state_year_prop_threshold=sy_p_t,mireps=False,summarize_sample=False)
+    mod_res = estimate.fit_model(estimate.get_estimation_sample(analytic_sample,['year','state','weekend','hour'],2),bsreps)
     res_fmt.append([yr,round(mod_res.final_params[0][0],2),'('+str(round(mod_res.final_params[0][1],2))+')',
                  round(mod_res.final_params[1][0],2),'('+str(round(mod_res.final_params[1][1],2))+')',(mod_res.df_resid+2)])
 res_fmt_df = pandas.DataFrame(res_fmt,columns=['year','theta','theta_se','lambda','lambda_se','total_dof'])
@@ -168,8 +169,9 @@ res_fmt_df.T.to_excel(results_folder + '\\figureA3_' + drink_def + '.xlsx') # No
 res_fmt = list() # list of results, formatted
 for yr in range(1983,1994): 
     print("Estimating multiple imputation model in year " + str(yr)) 
-    mod_res = lpFit.fit_model_mi(df_accident,df_vehicle,df_person,yr,yr,20,4,['year','state','weekend','hour'],'bac_test_primary',
-                        bac_threshold=0,state_year_prop_threshold=sy_p_t,bsreps=bsreps,mireps=mireps)
+    analytic_sample = util.get_analytic_sample(df_accident,df_vehicle,df_person,yr,yr,20,4,'bac_test_primary',
+                        bac_threshold=0,state_year_prop_threshold=sy_p_t,mireps=mireps,summarize_sample=False)
+    mod_res = estimate.fit_model_mi(analytic_sample,['year','state','weekend','hour'],2,bsreps,mireps)
     res_fmt.append([yr,round(mod_res.mi_params[0][0],2),'('+str(round(mod_res.mi_params[0][1],2))+')',
                      round(mod_res.mi_params[1][0],2),'('+str(round(mod_res.mi_params[1][1],2))+')',(mod_res.mi_df_resid+2)])
 res_fmt_df = pandas.DataFrame(res_fmt,columns=['year','theta','theta_se','lambda','lambda_se','total_dof'])
@@ -184,9 +186,9 @@ for earliest_hour_raw in range(20,29):
     else:
         earliest_hour = earliest_hour_raw    
     print("Estimating model for drinking definition " + drink_def + " in hour " + str(earliest_hour)) 
-    analytic_sample = lpUtil.get_analytic_sample(df_accident,df_vehicle,df_person,1983,1993,earliest_hour,earliest_hour,drink_def,
-                        bac_threshold=0,state_year_prop_threshold=sy_p_t,mireps=False,summarize_sample=False)
-    mod_res = lpFit.fit_model(analytic_sample,df_vehicle,df_person,['year','state','weekend','hour'],bsreps)
+    analytic_sample = util.get_analytic_sample(df_accident,df_vehicle,df_person,1983,1993,earliest_hour,earliest_hour,drink_def,
+                    bac_threshold=0,state_year_prop_threshold=sy_p_t,mireps=False,summarize_sample=False)
+    mod_res = estimate.fit_model(estimate.get_estimation_sample(analytic_sample,['year','state','weekend','hour'],2),bsreps)
     res_fmt.append([earliest_hour,round(mod_res.final_params[0][0],2),'('+str(round(mod_res.final_params[0][1],2))+')',
                  round(mod_res.final_params[1][0],2),'('+str(round(mod_res.final_params[1][1],2))+')',(mod_res.df_resid+2)])
 res_fmt_df = pandas.DataFrame(res_fmt,columns=['hour','theta','theta_se','lambda','lambda_se','total_dof'])
@@ -198,8 +200,9 @@ for earliest_hour_raw in range(20,29):
     else:
         earliest_hour = earliest_hour_raw    
     print("Estimating multiple imputation model in hour " + str(earliest_hour)) 
-    mod_res = lpFit.fit_model_mi(df_accident,df_vehicle,df_person,1983,1993,earliest_hour,earliest_hour,['year','state','weekend','hour'],'bac_test_primary',
-                        bac_threshold=0,state_year_prop_threshold=sy_p_t,bsreps=bsreps,mireps=mireps)
+    analytic_sample = util.get_analytic_sample(df_accident,df_vehicle,df_person,1983,1993,earliest_hour,earliest_hour,'bac_test_primary',
+                        bac_threshold=0,state_year_prop_threshold=sy_p_t,mireps=mireps,summarize_sample=False)
+    mod_res = estimate.fit_model_mi(analytic_sample,['year','state','weekend','hour'],2,bsreps,mireps)
     res_fmt.append([earliest_hour,round(mod_res.mi_params[0][0],2),'('+str(round(mod_res.mi_params[0][1],2))+')',
                      round(mod_res.mi_params[1][0],2),'('+str(round(mod_res.mi_params[1][1],2))+')',(mod_res.mi_df_resid+2)])
 res_fmt_df = pandas.DataFrame(res_fmt,columns=['hour','theta','theta_se','lambda','lambda_se','total_dof'])
