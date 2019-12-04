c06e4462bb69e1ed815d7b8de50ce4f7bb8479c6
King Richard the Patient <50369008+ephman4@users.noreply.github.com>
2019-12-03 20:37:48 -0500

    Update retrieve.py
    User modifications to retreive

--- retrieve.py
+++ retrieve.py
@@ -11,15 +11,38 @@ data folder for later extraction.
 # import necessary packages
 import ftplib, os, sys
 
+"""
+   USER-DEFINED ATTRIBUTES 
+   
+1) The years over which FARS datasets are retrieved. 
+    The default values are 1975 (the first year of FARS) to 2017 (the latest available year)
+    
+2) The directory to which the data folder containing the FARS data files will exist.
+    The user MUST set the 'parent' directory. 
+    There is NO DEFAULT. 
+    Failure to provide a parent will cause a syntax error
+    We recommend wherever the GitHub repository is cloned.
+    Data will then be placed into the subfolder .\GitHub\lpdt\data
+"""
+
+# FARS data range
+firstYear = 1975
+latestYear = 2017
+
+# Parent directory
+parent_dir = 
+
+
+""" Retrieval Script """
+
 # change working directory to GitHub path
-os.chdir(sys.path[0] + '\\Documents\\GitHub\\lp')
+if not os.path.exists(parent_dir + '\\GitHub\\lpdt'):
+        os.makedirs(parent_dir + '\\GitHub\\lpdt')
+os.chdir(parent_dir + '\\GitHub\\lpdt')
 
 # connect to NHTSA's FTP server
 ftp = ftplib.FTP('ftp.nhtsa.dot.gov')
 ftp.login()
-
-firstYear = 1975
-latestYear = 2017
 
 # retrieve each annual zipped files and store them in the data folder 
 for yr in range(firstYear,latestYear+1):
@@ -32,3 +55,5 @@ for yr in range(firstYear,latestYear+1):
     fileLocal = open(filenameLocal, 'wb')
     ftp.retrbinary('RETR FARS' + str(yr) + 'NationalCSV.zip', fileLocal.write)
     fileLocal.close()
+    
+print("Retrieval of FARS data from " + str(firstYear) + " to " + str(latestYear) + " successfully completed.")
