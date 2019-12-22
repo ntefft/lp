9c74db49bf9b57e7d291c87a6d38423c8a694638
King Richard the Patient <50369008+ephman4@users.noreply.github.com>
2019-12-21 17:00:32 -0800

    Added loops to repeated actions
    I made two broad changes--mostly playing really.
    
    First, I added some user options for years with defaults if user tries something silly. I could see two ways to improve it:
    
    a) forcing years to be integers for sake of completeneess
    b) the script checked the dataset where FARSXXXX were kept and found the smallest and largest values for XXXX and restricted years to fall between those values.
    
    Second, I added a dictionary (df_list) where keys were dataframe names. This allowed me to build some loops that allowed repeated operations to be swallowed. Now, the only actions that are dataset specific are the variable cleanings.

--- replication/extract.py
+++ replication/extract.py
@@ -8,130 +8,171 @@ This script extracts data from the raw FARS data files to be used for replicatin
 and the data definitions are harmonized across years. Accident, vehicle, and person dataframes are constructed and stored in csv files 
 for later use in the replication.
 """
-# import necessary packages
+
+# This script has been validated for FARS datasets from 1982 to 2017
+
+earliestYear=1982
+latestYear = 2017
+
+# install and import necessary packages
+    # not all packages are standard installs
+    # for example, us is not included in anaconda
+    # you may need to install some of these packages in the command line
+
 import os, numpy, pandas, shutil, us, zipfile
 
 """
    USER-DEFINED ATTRIBUTES 
       
-1) The working directory.
+1) The years over which FARS datasets are extracted and processed. 
+    The default values are 1982 to 2017 
+        **FARS begins in 1975, but the first year of imputed BAC is 1982
+          If the user-provided first year is before 1982:
+              -First year is reset to 1982
+              -A warning message will appear to alert the user, but the script will not break.
+         **FARS is updated annually, but this script is only validated through 2017
+          If the user-provided last year is after 2017:
+              -Last year is reset to 2017
+              -A warning message will appear to alert the user, but the script will not break.     
+          
+    
+2) The working directory.
     The user MUST set their own working directory before running the script. 
     We recommend the folder of the cloned GitHub repository.
         For example, set the working directory to "C:\\Users\\JoeEconomist\\GitHub\\lp"
-    Data will then be placed into the project subfolder .\replication\data
+    Data will then be placed into the subfolder .\replication\data
 """
+
+#os.chdir("C:\\Users\\dunnr\\Documents\\GitHub\\lpdt")
+
+# FARS data range
+firstYear = 1982
+lastYear = 2017
+
+
+if firstYear>lastYear:
+    print('User selected lastYear earlier than firstYear. firstYear has been set to ' + str(earliestYear) + ' and lastYear has been set to ' + str(latestYear) +'.')
+    firstYear = earliestYear
+    lastYear = latestYear
+if firstYear < earliestYear:
+    print('User selected firstYear prior to ' + str(earliestYear) + '. firstYear has been set to ' + str(earliestYear) +'.')
+    firstYear = earliestYear
+if lastYear > latestYear:
+    print('User selected lastYear after ' + str(latestYear) + '. lastYear has been set to ' + str(latestYear) +'.')
+    lastYear = latestYear
 
 # load US state abbreviations for later merge
 df_states = pandas.DataFrame.from_dict(us.states.mapping('fips', 'abbr'),orient='index',columns=['state_abbr'])
 df_states = df_states[df_states.index.notnull()]
 df_states.index = df_states.index.astype(int)
 
-firstYear = 1982 # 1982 is the first for which multiple imputation files are available
-latestYear = 2017
+# Initialize analytic dataframe for the accident, vehicle, and person datasets
 
-df_accident = pandas.DataFrame() # initialize analytic dataframe
-df_vehicle = pandas.DataFrame() # initialize analytic dataframe
-df_person = pandas.DataFrame() # initialize analytic dataframe
+#fars_datasets = ['accident', 'vehicle', 'person', 'Miper']
 
+fars_datasets = ['accident', 'vehicle', 'person', 'Miper']
+dataset_ids = ['st_case','veh_no','per_no', 'per_no']
+
+df_list={}
+for dataset in fars_datasets:
+    df_list[dataset]=pandas.DataFrame()
+    
 # loop over years to be included in the replication analysis
-for yr in range(firstYear,latestYear+1): 
+for yr in range(firstYear,lastYear+1): 
     print('Extracting data from ' + str(yr) + '.' )
     
     # extract accident, vehicle, person, and multiple imputation files
     zipfile.ZipFile('data\\FARS' + str(yr) + '.zip', 'r').extractall(path='data\\extracted')
     # UTF-8 encoding errors are ignored because they don't impact the relevant variables
-    acc_file = open('data\\extracted\\accident.csv', errors='ignore')
-    df_acc_yr = pandas.read_csv(acc_file)
-    acc_file.close()
-    veh_file = open('data\\extracted\\vehicle.csv',errors='ignore')
-    df_veh_yr = pandas.read_csv(veh_file) 
-    veh_file.close()
-    df_per_yr = pandas.read_csv('data\\extracted\\person.csv')
-    df_mi_yr = pandas.read_csv('data\\extracted\\Miper.csv')    
-    
-    for ft in ['acc','veh','per','mi']:
-        vars()['df_' + ft + '_yr'].columns = vars()['df_' + ft + '_yr'].columns.str.lower() # convert all columns to lower case    
-    shutil.rmtree(path='data\\extracted') # clean up temporary extractions folder
-
-    # clean accident variables, e.g. harmonize names, ensure correct datatypes, and record missing variables 
-    df_acc_yr['st_case'] = df_acc_yr['st_case'].astype('int')
-    df_acc_yr.set_index([numpy.full(len(df_acc_yr.index), yr),'st_case'],inplace=True) # set the multiindex as year and st_case
-    df_acc_yr.index.set_names(['year','st_case'], inplace=True) # set the multiindex names
-    df_acc_yr.loc[df_acc_yr.hour==99, 'hour'] = numpy.nan
-    df_acc_yr.loc[df_acc_yr.hour==24, 'hour'] = 0
-    df_acc_yr.loc[df_acc_yr.day_week==9, 'day_week'] = numpy.nan
-    df_acc_yr['quarter'] = numpy.ceil(df_acc_yr['month']/3) # create quarter variable
-    df_acc_yr = df_acc_yr.merge(df_states,how='left',left_on='state',right_index=True) # merge in state abbreviations
-    
-    # keep relevant accident variables and append to accident dataframe
-    df_acc_yr = df_acc_yr[['state','state_abbr','quarter','day_week','hour','persons']]
-    print('Count of accidents: ' + str(len(df_acc_yr)))
-    df_accident = df_accident.append(df_acc_yr)
-
-    # clean vehicle variables, e.g. harmonize names, ensure correct datatypes, and record missing variables 
-    df_veh_yr[['st_case','veh_no']] = df_veh_yr[['st_case','veh_no']].astype('int')
-    df_veh_yr.set_index([numpy.full(len(df_veh_yr.index), yr),'st_case','veh_no'],inplace=True) # set the multiindex as year, st_case, and veh_no
-    df_veh_yr.index.set_names(['year','st_case','veh_no'], inplace=True) # set the multiindex names
-    if yr <= 2008: 
-        df_veh_yr['occupants'] = df_veh_yr['ocupants']
-    else:
-        df_veh_yr['occupants'] = df_veh_yr['numoccs']
-    if yr <= 2015:
-        df_veh_yr.loc[df_veh_yr.occupants>=99, 'occupants'] = numpy.nan
-    else:
-        df_veh_yr.loc[df_veh_yr.occupants>=97, 'occupants'] = numpy.nan	
-    for vt in ['acc','sus','dwi','spd','oth']:
-        df_veh_yr.loc[df_veh_yr['prev_' + vt] > 97, 'prev_' + vt] = numpy.nan # previous violations
         
-    # keep relevant vehicle variables and append to vehicle dataframe
-    df_veh_yr = df_veh_yr[['prev_acc','prev_sus','prev_dwi','prev_spd','prev_oth','dr_drink','occupants']]
-    print('Count of vehicles: ' + str(len(df_veh_yr)))
-    df_vehicle = df_vehicle.append(df_veh_yr)
+    df_list_yr={}
+    index_list=['year']
+    for (dataset, id) in zip(fars_datasets,dataset_ids):
+        file = open('data\\extracted\\' + dataset + '.csv', errors='ignore')
+        df_list_yr[dataset]=pandas.read_csv(file)
+        file.close()
+    
+        df_list_yr[dataset].columns = df_list_yr[dataset].columns.str.lower() # make all columns lowercase
+        df_list_yr[dataset]['year']=numpy.full(len(df_list_yr[dataset].index), yr) # standardize the year variable to 4 digits
+        
+        if not dataset == 'Miper':
+            index_list.append(id)
+        df_list_yr[dataset][index_list] = df_list_yr[dataset][index_list].astype('int') # set the indices as integers
+        df_list_yr[dataset].set_index(index_list, inplace=True) # set the multiindex
+        df_list_yr[dataset].index.set_names(index_list, inplace=True)  
 
-    # clean person variables, e.g. harmonize names, ensure correct datatypes, and record missing variables 
-    df_per_yr[['st_case','veh_no','per_no']] = df_per_yr[['st_case','veh_no','per_no']].astype('int')
-    df_per_yr.set_index([numpy.full(len(df_per_yr.index), yr),'st_case','veh_no','per_no'],inplace=True) # set the multiindex as year, st_case, veh_no, per_no 
-    df_per_yr.index.set_names(['year','st_case','veh_no','per_no'], inplace=True) # set the multiindex names           
-    if yr <= 1990: # alcohol test results
-        df_per_yr['alcohol_test_result'] = df_per_yr['test_res']
+    shutil.rmtree(path='data\\extracted') # clean up temporary extractions folder
+    
+    # Manipulating accident data
+    df_list_yr['accident'].loc[df_list_yr['accident'].hour==99, 'hour'] = numpy.nan
+    df_list_yr['accident'].loc[df_list_yr['accident'].hour==24, 'hour'] = 0
+    df_list_yr['accident'].loc[df_list_yr['accident'].day_week==9, 'day_week'] = numpy.nan
+    df_list_yr['accident']['quarter'] = numpy.ceil(df_list_yr['accident']['month']/3) # create quarter variable
+    df_list_yr['accident'] = df_list_yr['accident'].merge(df_states,how='left',left_on='state',right_index=True) # merge in state abbreviations
+
+    # keep relevant accident variables and append to accident dataframe
+    df_list_yr['accident'] = df_list_yr['accident'][['state','state_abbr','quarter','day_week','hour','persons']]
+    print('Count of crashes: ' + str(len(df_list_yr['accident'])))
+    df_list['accident'] = df_list['accident'].append(df_list_yr['accident'])
+    
+    # Manipulating vehicle data
+    if yr <= 2008: 
+        df_list_yr['vehicle']['occupants'] = df_list_yr['vehicle']['ocupants']
     else:
-        df_per_yr['alcohol_test_result'] = df_per_yr['alc_res']
+        df_list_yr['vehicle']['occupants'] = df_list_yr['vehicle']['numoccs']
+    if yr <= 2015:
+        df_list_yr['vehicle'].loc[df_list_yr['vehicle'].occupants>=99, 'occupants'] = numpy.nan
+    else:
+        df_list_yr['vehicle'].loc[df_list_yr['vehicle'].occupants>=97, 'occupants'] = numpy.nan	
+    for vt in ['acc','sus','dwi','spd','oth']:
+        df_list_yr['vehicle'].loc[df_list_yr['vehicle']['prev_' + vt] > 97, 'prev_' + vt] = numpy.nan # previous violations
+
+    # keep relevant vehicle variables and append to vehicle dataframe
+    df_list_yr['vehicle'] = df_list_yr['vehicle'][['prev_acc','prev_sus','prev_dwi','prev_spd','prev_oth','dr_drink','occupants']]
+    print('Count of vehicles: ' + str(len(df_list_yr['vehicle'])))
+    df_list['vehicle'] = df_list['vehicle'].append(df_list_yr['vehicle'])
+    
+    # Manipulating person variables
+    
+    #standardize alcohol test result
+    if yr <= 1990: 
+        df_list_yr['person']['alcohol_test_result'] = df_list_yr['person']['test_res']
+    else:
+        df_list_yr['person']['alcohol_test_result'] = df_list_yr['person']['alc_res']
+    
     if yr >= 2015:
-        df_per_yr['alcohol_test_result'] = df_per_yr['alcohol_test_result']/10
-    df_per_yr.loc[df_per_yr.alcohol_test_result>=95, 'alcohol_test_result'] = numpy.nan    
+        df_list_yr['person']['alcohol_test_result'] = df_list_yr['person']['alcohol_test_result']/10
+    
+    df_list_yr['person'].loc[df_list_yr['person'].alcohol_test_result>=95, 'alcohol_test_result'] = numpy.nan    
+    
+    
     for vn in ['alc_det','atst_typ','race']: # create variables if necessary
-        if vn not in df_per_yr.columns:
-            df_per_yr[vn] = numpy.nan
+        if vn not in df_list_yr['person'].columns:
+            df_list_yr['person'][vn] = numpy.nan
+    
     if yr <= 2008:
-        df_per_yr.loc[df_per_yr.age==99, 'age'] = numpy.nan # age
+        df_list_yr['person'].loc[df_list_yr['person'].age==99, 'age'] = numpy.nan # age
     else:
-        df_per_yr.loc[df_per_yr.age>=998, 'age'] = numpy.nan # age
-    df_per_yr['age_lt15'] = df_per_yr['age'] < 15 # less than 15 defined as child for our purposes
-    df_per_yr.loc[df_per_yr.sex.isin([8,9]), 'sex'] = numpy.nan # sex
-    df_per_yr.loc[df_per_yr.race==99, 'race'] = numpy.nan # race
-    df_per_yr.loc[df_per_yr.seat_pos>=98, 'seat_pos'] = numpy.nan # seat position
+        df_list_yr['person'].loc[df_list_yr['person'].age>=998, 'age'] = numpy.nan # age
+    
+    df_list_yr['person']['age_lt15'] = df_list_yr['person']['age'] < 15 # less than 15 defined as child for our purposes
+    df_list_yr['person'].loc[df_list_yr['person'].sex.isin([8,9]), 'sex'] = numpy.nan # sex
+    df_list_yr['person'].loc[df_list_yr['person'].race==99, 'race'] = numpy.nan # race
+    df_list_yr['person'].loc[df_list_yr['person'].seat_pos>=98, 'seat_pos'] = numpy.nan # seat position
     
     # clean mulptiple imputation variables, e.g. harmonize names, ensure correct datatypes, and record missing variables 
-    df_mi_yr[['st_case','veh_no','per_no']] = df_mi_yr[['st_case','veh_no','per_no']].astype('int')
-    if 'year' in df_mi_yr.columns: # preparing to reset the index
-       df_mi_yr.drop(columns='year',inplace=True) 
-    df_mi_yr.set_index([numpy.full(len(df_mi_yr.index), yr),'st_case','veh_no','per_no'],inplace=True) # set the multiindex as year, st_case, veh_no, per_no 
-    df_mi_yr.index.set_names(['year','st_case','veh_no','per_no'], inplace=True)
-    df_mi_yr = df_mi_yr.rename(columns={'p1':'mibac1','p2':'mibac2','p3':'mibac3','p4':'mibac4','p5':'mibac5','p6':'mibac6','p7':'mibac7','p8':'mibac8','p9':'mibac9','p10':'mibac10'}) # rename bac columns    
-    df_per_yr = df_per_yr.merge(df_mi_yr,how='left',on=['year','st_case','veh_no','per_no']) # merge multiply imputed bac values into person dataframe
+    df_list_yr['Miper'] = df_list_yr['Miper'].rename(columns={'p1':'mibac1','p2':'mibac2','p3':'mibac3','p4':'mibac4','p5':'mibac5','p6':'mibac6','p7':'mibac7','p8':'mibac8','p9':'mibac9','p10':'mibac10'}) # rename bac columns    
+    df_list_yr['person'] = df_list_yr['person'].merge(df_list_yr['Miper'],how='left',on=['year','st_case','veh_no','per_no']) # merge multiply imputed bac values into person dataframe
     
     # keep relevant person variables and append to person dataframe
-    df_per_yr = df_per_yr[['seat_pos','drinking','alc_det','atst_typ','alcohol_test_result','race','age','age_lt15','sex','mibac1','mibac2','mibac3','mibac4','mibac5','mibac6','mibac7','mibac8','mibac9','mibac10']]
-    print('Count of persons: ' + str(len(df_per_yr)))
-    df_person = df_person.append(df_per_yr)
+    df_list_yr['person'] = df_list_yr['person'][['seat_pos','drinking','alc_det','atst_typ','alcohol_test_result','race','age','age_lt15','sex','mibac1','mibac2','mibac3','mibac4','mibac5','mibac6','mibac7','mibac8','mibac9','mibac10']]
+    print('Count of persons: ' + str(len(df_list_yr['person'])))
+    df_list['person'] = df_list['person'].append(df_list_yr['person'])
     
-    for ft in ['acc','veh','per','mi']: # clean up memory 
-        del vars()['df_' + ft + '_yr']
-
 # summarize the constructed dataframes and save to csv files
 if not os.path.exists('replication\\data'):
     os.makedirs('replication\\data')
-for dfn in ['df_accident','df_vehicle','df_person']:
+for dfn in ['accident', 'vehicle', 'person']:
     print('Describing dataframe ' + dfn)
-    print(vars()[dfn].describe())
-    vars()[dfn].to_csv('replication\\data\\' + dfn + '.csv')
+    print(df_list[dfn].describe())
+    df_list[dfn].to_csv('replication\\data\\df_' + dfn + '.csv')
